123a490a12cd28cf82de59e24d76469605034577
隈本 次信 <jambokumamoto@gmail.com>
2020-05-09 14:25:59 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+# ページごとにタイトルを返す
+  def full_title(page_name = "")　# メソッドと引数の定義
+    base_title = "AttendanceApp" # 基本となるアプリーケーション名を変数にだいにゅ
+    if page_name.empty? # 引数を受けとっているか判定
+      base_title # 引数oage_nameが空文字でない場合
+    else
+      page_name + " | " + base_title # 文字列を連結して返す
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
+  root 'static_pages#top'
 
 
   
