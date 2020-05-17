6d7eb37b1f6b84966527135def1733abc8c91184
隈本 次信 <jambokumamoto@gmail.com>
2020-05-17 16:21:28 +0000

    在社時間を計算

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -9,4 +9,10 @@ module AttendancesHelper
     # どれにも当てはまらなかった場合はfalseを返します。
     false
   end
+  
+  
+  # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
+  def working_times(start, finish)
+    format("%.2f", (((finish - start) / 60) / 60.0))
+  end
 end
